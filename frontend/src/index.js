26784bfddc3e4aa1089a592515509104f85fbc14
Tommy Hang <tommyhang@berkeley.edu>
2023-05-03 15:05:51 -0700

    working on webprovider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+function getLibrary(provider) {
+  return new Web3Provider(provider);
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}><App /></Web3ReactProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
